a7757481311fd76a2af92b20c03f7ba05623dbe4
fernandafernandes2903-arch <fernandafernandes2903@gmail.com>
2026-07-13 19:51:46 +0000

    criei a pasta unit e coloquei o arquivo calculadora.test nele

--- tests/unit/calculadora.tests.js
+++ tests/unit/calculadora.tests.js
@@ -1,6 +1,6 @@
 
 
-const caluladora =  require("../models/calculadora.js");
+const caluladora =  require("../../models/calculadora.js");
 test ('Soma de 2 + 2 deve ser igual a 4', () => {
   const resultado = calculadora.somar(2, 2);
   expect(resultado).toBe(4);
@@ -9,4 +9,9 @@ test ('Soma de 2 + 2 deve ser igual a 4', () => {
 test ('Soma de 5 + 100 deve ser igual a 105', () => {
   const resultado = calculadora.somar(5, 100);
   expect(resultado).toBe(105);
+});
+
+test ('Soma de banana + 100 deve ser igual a error', () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe(Error);
 });
